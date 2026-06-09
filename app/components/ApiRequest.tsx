03104aca11386b7c2a1a1c4ed9b51d60dff7abb1
Abdul-Rahman-Rafat <abdorafat489@gmail.com>
2026-06-09 07:46:17 +0300

    api (client-side) + (server-side >> ssr + ssg + ISR )

---
+++ app/components/ApiRequest.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { useState, useEffect } from "react";
+export default function ApiRequest() {
+  // تعريف الـ State لتخزين البيانات
+  const [todo, setTodo] = useState({});
+
+  useEffect(() => {
+    // وظيفة لجلب البيانات من API خارجي
+    const fetchTodo = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos/1",
+      );
+      const result = await response.json();
+      setTodo(result); // تحديث الحالة بالبيانات الجديدة
+    };
+
+    fetchTodo();
+  }, []); // مصفوفة فارغة لضمان تشغيل الكود مرة واحدة عند تحميل المكون
+  console.log(todo);
+  return (
+    <>
+      <h1>{todo.title}</h1>
+    </>
+  );
+}
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,3 +1,28 @@
-export default function postsPage() {
-  return <h1> posts Page</h1>;
+import ApiRequest from "../components/ApiRequest";
+export default async function postsPage() {
+  // الميزة في الssr
+  // هنكتب الapi  على طول بدون useEffect
+
+  // 1. جلب البيانات مباشرة باستخدام await داخل المكون [5]
+  // نستخدم هنا خيار revalidate لتفعيل تقنية ISR (التحديث الدوري) [9]
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    cache: "force-cache", // SSG
+    //  cache:"no-cache" // SSR
+  });
+
+  const post = await response.json();
+
+  return (
+    <>
+      {" "}
+      <h1>posts Page</h1>
+      <div>
+        <ApiRequest />
+      </div>
+      <div>
+        <h1>server side</h1>
+        <h2>post.title</h2>
+      </div>
+    </>
+  );
 }
